4ecead5ccf70a885fcb83103a68d89f7d8f36d9f
parktrue <pomul@naver.com>
2023-08-20 21:18:42 +0900

    chore : 서블릿 API 사용

---
+++ project/upgradeList/app-57/app-server/src/main/java/bitcamp/myapp/handler/SoldierDeleteServlet.java
@@ -0,0 +1,40 @@
+package bitcamp.myapp.handler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bitcamp.myapp.vo.Soldier;
+
+@WebServlet("/soldier/delete")
+public class SoldierDeleteServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    Soldier loginUser = (Soldier) request.getSession().getAttribute("loginUser");
+    if (loginUser == null || !loginUser.getMilitaryNumber().equals("07-72000001")) {
+      response.sendRedirect("/auth/form.html");
+      return;
+    }
+
+    try {
+      if (InitServlet.soldierDao.delete(Integer.parseInt(request.getParameter("no"))) == 0) {
+        throw new Exception("해당 번호의 회원이 없습니다.");
+      } else {
+        response.sendRedirect("/soldier/list");
+      }
+      InitServlet.sqlSessionFactory.openSession(false).commit();
+
+    } catch (Exception e) {
+      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      throw new RuntimeException(e);
+    }
+  }
+
+}
