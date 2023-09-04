ac165f12e9ecc48f9872ab343038556f04ffcda0
parktrue <pomul@naver.com>
2023-09-05 00:48:41 +0900

    feat : 프록시 패턴 기술을 사용하여 트랜잭션 코드를 삽입
    애노테이션을 사용하여 트랜잭션 제어
    버전73

---
+++ project/upgradeList/app-73/app-server/src/main/java/bitcamp/myapp/controller/SoldierListController.java
@@ -0,0 +1,22 @@
+package bitcamp.myapp.controller;
+
+import bitcamp.myapp.service.SoldierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("/soldier/list")
+public class SoldierListController implements PageController {
+
+  @Autowired
+  SoldierService soldierService;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    soldierService.updateDday();
+    request.setAttribute("list", soldierService.list());
+    return "/WEB-INF/jsp/soldier/list.jsp";
+  }
+}
